2516d03514e0aa46a526451d5a719119fe851365
Artii15 <artur.kowalski15@gmail.com>
2016-04-22 21:02:31 +0200

    Dhcp message struct

---
+++ inc/dhcp_message.h
@@ -0,0 +1,29 @@
+#ifndef DHCP_MESSAGE_H
+#define DHCP_MESSAGE_H
+
+#include <stdint.h>
+
+#define MAX_HADDR_SIZE 16
+#define MAX_SERVER_NAME_SIZE 64
+#define MAX_BOOT_FILE_NAME_SIZE 128
+#define MAX_OPTIONS_SIZE 312
+
+struct DHCPMessage {
+	uint8_t op;
+	uint8_t htype;
+	uint8_t hlen;
+	uint8_t hops;
+	uint32_t xid;
+	uint16_t secs;
+	uint16_t flags;
+	uint32_t ciaddr;
+	uint32_t yiaddr;
+	uint32_t siaddr;
+	uint32_t giaddr;
+	uint8_t chaddr[MAX_HADDR_SIZE];
+	uint8_t sname[MAX_SERVER_NAME_SIZE];
+	uint8_t file[MAX_BOOT_FILE_NAME_SIZE];
+	uint8_t options[MAX_OPTIONS_SIZE];
+}
+
+#endif
