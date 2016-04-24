508eef2aaf75f13c61ca2e494cbf803177572ae3
Artii15 <artur.kowalski15@gmail.com>
2016-04-24 21:32:08 +0200

    addresses pool

--- inc/addresses_pool.h
+++ inc/addresses_pool.h
@@ -1,8 +1,17 @@
 #ifndef ADDRESSES_POOL_H
 #define ADDRESSES_POOL_H
 
+#include <stdint.h>
+#include <set>
+
 class AddressesPool {
-	AddressesPool(const char* poolFilePath);
+	public:
+		uint32_t startAddress;
+		uint32_t endAddress;
+		uint32_t lastAssigned;
+		uint32_t networkMask;
+		
+		std::set<uint32_t> addressesInUse;
 };
 
 #endif
