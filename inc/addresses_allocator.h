fb1c197cb8ae11588af29c72237f46fddb991ee0
Artii15 <artur.kowalski15@gmail.com>
2016-04-24 21:13:51 +0200

    Helper classes

--- inc/addresses_allocator.h
+++ inc/addresses_allocator.h
@@ -3,10 +3,14 @@
 
 #include "hardware_address.h"
 #include "allocated_address.h"
+#include "config.h"
+#include "addresses_pool.h"
 #include <stdint.h>
 
 class AddressesAllocator {
 	public:
+		AddressesAllocator(Config& config, AddressesPool& addressesPool);
+
 		AllocatedAddress allocate();
 		AllocatedAddress allocate(uint32_t networkAddress);
 		AllocatedAddress allocate(const HardwareAddress& clientAddress, uint32_t networkAddress);
---
+++ inc/addresses_pool.h
@@ -0,0 +1,8 @@
+#ifndef ADDRESSES_POOL_H
+#define ADDRESSES_POOL_H
+
+class AddressesPool {
+	AddressesPool(const char* poolFilePath);
+};
+
+#endif
