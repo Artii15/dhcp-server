371609f6738fcad89c35a7d668b5f640aa571fb3
Artii15 <artur.kowalski15@gmail.com>
2016-04-24 22:26:55 +0200

    Reading start address

--- src/addresses_pool.cpp
+++ src/addresses_pool.cpp
@@ -1,7 +1,19 @@
 #include "../inc/addresses_pool.h"
+#include <sstream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+
+using namespace std;
 
 AddressesPool::AddressesPool(const char* poolString) {
+	// Example pool string: 192.168.1.2:192.168.1.254:255.255.255.0:24
+	istringstream segments(poolString);
 
+	string buffer;
+	getline(segments, buffer, ':');
+	inet_aton(buffer.c_str(), (struct in_addr*)&startAddress);
 }
 
 uint32_t AddressesPool::getNext() {
