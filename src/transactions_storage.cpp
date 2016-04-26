05df757dac98a3f5b4aef3d90033a9635a61dd5e
Artii15 <artur.kowalski15@gmail.com>
2016-04-26 21:30:29 +0200

    Task data now includes timer

--- src/transactions_storage.cpp
+++ src/transactions_storage.cpp
@@ -6,20 +6,37 @@
 
 using namespace std;
 
+struct CleanTaskData {
+	uint32_t transactionId;
+	unordered_map<uint32_t, Transaction>* transactions;
+	timer_t timer;
+};
+
 TransactionsStorage::TransactionsStorage(Config& configuration): config(configuration) {}
+
+void deleteOldTransaction(union sigval data) {
+	CleanTaskData* taskData = (CleanTaskData*)data.sival_ptr;
+	taskData->transactions->erase(taskData->transactionId);
+	
+	delete taskData;
+}
 
 void TransactionsStorage::storeTransaction(const Transaction& transaction) {
 	transactions[transaction.id] = transaction;
 
-	timer_t timer;
+	CleanTaskData* taskData = new CleanTaskData();
+	taskData->transactionId = transaction.id;
+	taskData->transactions = &transactions;
+
 	struct sigevent onTick;
 	memset(&onTick, 0, sizeof(onTick));
 	onTick.sigev_notify = SIGEV_THREAD;
 	onTick.sigev_signo = 0;
-	onTick.sigev_value.sival_int = transaction.id;
-	onTick.sigev_notify_function = NULL;
 
-	if(timer_create(CLOCK_MONOTONIC, &onTick, &timer) < 0) {
+	onTick.sigev_value.sival_ptr = taskData;
+	onTick.sigev_notify_function = deleteOldTransaction;
+
+	if(timer_create(CLOCK_MONOTONIC, &onTick, &taskData->timer) < 0) {
 		throw runtime_error("Could not create clock for transaction delete task");
 	}
 }
