612016a52b55e72fd28e1337c9939a3dd9bfa557
Artii15 <artur.kowalski15@gmail.com>
2016-04-26 21:50:32 +0200

    Storage tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "../inc/config.h"
 #include "../inc/addresses_allocator.h"
 #include "../inc/server.h"
+#include "../inc/transactions_storage.h"
 
 int main(int argc, char** argv) {
 	Config config("config.json");
+	TransactionsStorage storage(config);
+
 	AddressesAllocator allocator(config);
 	Server server(allocator, config);
 
