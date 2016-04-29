26c967409776edd72ed350c87f590f8b367231cb
Artii15 <artur.kowalski15@gmail.com>
2016-04-29 21:46:57 +0200

    Generic packing methods

--- src/packer.cpp
+++ src/packer.cpp
@@ -1,13 +1,35 @@
 #include "../inc/packer.h"
+#include <string.h>
+
+using namespace std;
 
 uint8_t* Packer::pack(uint8_t* dst, uint8_t optionType, uint32_t value) {
-	return 0;
+	*(dst++) = optionType;
+	*(dst++) = sizeof(value);
+	*(dst++) = value;
+
+	return dst;
 }
 
 uint8_t* Packer::pack(uint8_t* dst, uint8_t optionType, uint8_t value) {
-	return 0;
+	*(dst++) = optionType;
+	*(dst++) = sizeof(value);
+	*(dst++) = value;
+
+	return dst;
 }
 
 uint8_t* Packer::pack(uint8_t* dst, uint8_t optionType, const std::list<uint32_t>* value) {
-	return 0;
+	if(!value->empty()) {
+		*(dst++) = optionType;
+		*(dst++) = value->size() * sizeof(uint32_t);
+
+		for(list<uint32_t>::const_iterator it = value->begin(); it != value->end(); it++) {
+			uint32_t element = *it;
+			memcpy(dst, &element, sizeof(element));
+			dst += sizeof(element);
+		}
+	}
+
+	return dst;
 }
