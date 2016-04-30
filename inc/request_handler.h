bd24d40182a9d767d65ee52c296afe83fe442c4b
Artii15 <artur.kowalski15@gmail.com>
2016-04-30 10:41:20 +0200

    Checking server ip

--- inc/request_handler.h
+++ inc/request_handler.h
@@ -5,10 +5,13 @@
 #include "client.h"
 #include "transactions_storage.h"
 #include "addresses_allocator.h"
+#include "dhcp_message.h"
+#include "options.h"
 
 class RequestHandler {
 	public:
 		RequestHandler(TransactionsStorage&, Client&, AddressesAllocator&, Server&);
+		void handle(struct DHCPMessage&, Options&);
 
 	private:
 		TransactionsStorage& transactionsStorage;
--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -2,3 +2,10 @@
 
 RequestHandler::RequestHandler(TransactionsStorage& storage, Client& clientToHandle, AddressesAllocator& addrAllocator, Server& serv)
 	: transactionsStorage(storage), client(clientToHandle), allocator(addrAllocator), server(serv) {}
+
+
+void RequestHandler::handle(struct DHCPMessage& request, Options& options) {
+	if(*options.get(SERVER_IDENTIFIER).value == server.serverIp) {
+
+	}
+}
