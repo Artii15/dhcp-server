2624c1f9c570bba8a643e84ea523706e1a7fea33
Artii15 <artur.kowalski15@gmail.com>
2016-05-01 13:16:09 +0200

    Two way conversion

--- inc/options.h
+++ inc/options.h
@@ -14,10 +14,16 @@ class Options {
 
 	private:
 		void read(uint8_t* rawOptions, unsigned maxOptionsLength);
+
 		void tryToSetHostBytesOrder(Option&);
 		void toHost32(Option&);
 		void toHost32(uint8_t* bytes);
 		void toHostArray32(Option&);
+
+		void tryToSetNetworkBytesOrder(Option&);
+		void toNetwork32(Option&);
+		void toNetwork32(uint8_t* bytes);
+		void toNetworkArray32(Option&);
 
 
 		std::unordered_map<uint8_t, Option> options;
--- src/options.cpp
+++ src/options.cpp
@@ -59,10 +59,6 @@ void Options::tryToSetHostBytesOrder(Option& option) {
 	}
 }
 
-void Options::toNetworkReprezentation() {
-
-}
-
 void Options::toHostArray32(Option& option) {
 	for(unsigned i = 0; i < option.length; i += sizeof(uint32_t)) {
 		toHost32(option.value + i*sizeof(uint32_t));
@@ -76,6 +72,53 @@ void Options::toHost32(Option& option) {
 void Options::toHost32(uint8_t* bytes) {
 	uint32_t hostValue = ntohl((uint32_t)*bytes);
 	memcpy(bytes, &hostValue, sizeof(hostValue));
+}
+
+void Options::toNetworkReprezentation() {
+	for(unordered_map<uint8_t, Option>::iterator it = options.begin(); it != options.end(); it++) {
+		Option& option = it->second;
+		tryToSetNetworkBytesOrder(option);
+	}
+}
+
+void Options::tryToSetNetworkBytesOrder(Option& option) {
+	switch(option.code) {
+		case SUBNET_MASK: 
+			toNetwork32(option);
+			break;
+		case ROUTERS: 
+			toNetworkArray32(option);
+			break;
+		case DNS_OPTION: 
+			toNetworkArray32(option);
+			break;
+		case REQUESTED_IP_ADDRESS: 
+			toNetwork32(option);
+			break;
+		case IP_ADDRESS_LEASE_TIME: 
+			toNetwork32(option);
+			break;
+		case SERVER_IDENTIFIER: 
+			toNetwork32(option);
+			break;
+		default:
+			break;
+	}
+}
+
+void Options::toNetworkArray32(Option& option) {
+	for(unsigned i = 0; i < option.length; i += sizeof(uint32_t)) {
+		toNetwork32(option.value + i*sizeof(uint32_t));
+	}
+}
+
+void Options::toNetwork32(Option& option) {
+	toNetwork32(option.value);
+}
+
+void Options::toNetwork32(uint8_t* bytes) {
+	uint32_t networkValue = htonl((uint32_t)*bytes);
+	memcpy(bytes, &networkValue, sizeof(networkValue));
 }
 
 Option& Options::get(uint8_t code) {
